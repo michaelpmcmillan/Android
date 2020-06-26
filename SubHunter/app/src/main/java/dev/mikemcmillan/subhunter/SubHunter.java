d9271dafe13e8e046053d9d2f3262f28d4b9af6a
Mike <mike.mcmillan@onthebeach.co.uk>
2020-06-26 13:41:43 +0100

    chapter3 - add variables and debug logging

--- SubHunter/app/src/main/java/dev/mikemcmillan/subhunter/SubHunter.java
+++ SubHunter/app/src/main/java/dev/mikemcmillan/subhunter/SubHunter.java
@@ -3,23 +3,54 @@ package dev.mikemcmillan.subhunter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
 
 public class SubHunter extends Activity {
+
+    int numberHorizontalPixels;
+    int numberVerticalPixels;
+    int blockSize;
+    int gridWidth = 40;
+    int gridHeight;
+    float horizontalTouched = -100;
+    float verticalTouched = -100;
+    int subHorizontalPosition;
+    int subVerticalPosition;
+    boolean hit = false;
+    int shotsTaken;
+    int distanceFromSub;
+    boolean debugging = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Log.d("Debugging", "In onCreate");
+
+        getDeviceScreenResolution();
+
         newGame();
         draw();
     }
 
+    private void getDeviceScreenResolution() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        numberHorizontalPixels = size.x;
+        numberVerticalPixels = size.y;
+        blockSize = numberHorizontalPixels / gridWidth;
+        gridHeight = numberVerticalPixels / blockSize;
+    }
+
     private void draw() {
         Log.d("Debugging", "In draw");
+        printDebuggingText();
     }
 
     private void newGame() {
@@ -43,6 +74,16 @@ public class SubHunter extends Activity {
     }
 
     private void printDebuggingText() {
+        Log.d("numberHorizontalPixels", "" + numberHorizontalPixels);
+        Log.d("numberVerticalPixels", "" + numberVerticalPixels);
 
+        Log.d("blockSize", "" + blockSize);
+        Log.d("gridWidth", "" + gridWidth);
+        Log.d("gridHeight", "" + gridHeight);
+
+        Log.d("horizontalTouched", "" + horizontalTouched);
+        Log.d("verticalTouched", "" + verticalTouched);
+        Log.d("subHorizontalPosition", "" + subHorizontalPosition);
+        Log.d("subVerticalPosition", "" + subVerticalPosition);
     }
 }
