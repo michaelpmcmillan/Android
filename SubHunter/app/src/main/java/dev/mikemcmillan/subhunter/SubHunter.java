883f34518687cd16d6886586d2df91c53d949603
Mike <mike.mcmillan@onthebeach.co.uk>
2020-06-26 08:33:01 +0100

    Create project

---
+++ SubHunter/app/src/main/java/dev/mikemcmillan/subhunter/SubHunter.java
@@ -0,0 +1,15 @@
+package dev.mikemcmillan.subhunter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubHunter extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
