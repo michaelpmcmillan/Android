89645deb96dbac3b16b498b636413282dae2cb3f
Mike <mike.mcmillan@onthebeach.co.uk>
2020-06-26 13:23:46 +0100

    chapter2 - add game methods

--- SubHunter/app/src/main/java/dev/mikemcmillan/subhunter/SubHunter.java
+++ SubHunter/app/src/main/java/dev/mikemcmillan/subhunter/SubHunter.java
@@ -4,12 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class SubHunter extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Log.d("Debugging", "In onCreate");
+        newGame();
+        draw();
+    }
+
+    private void draw() {
+        Log.d("Debugging", "In draw");
+    }
+
+    private void newGame() {
+        Log.d("Debugging", "In newGame");
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d("Debugging", "In onTouchEvent");
+        takeShot();
+        return true;
+    }
+
+    private void takeShot() {
+        Log.d("Debugging", "In onTouchEvent");
+        draw();
+    }
+
+    private void boom() {
+
+    }
+
+    private void printDebuggingText() {
+
     }
 }
